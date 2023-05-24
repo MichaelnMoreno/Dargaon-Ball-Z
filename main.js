fa19d33ce81adb1d973c775b5ca6c3bf32f53967
Michael Moreno <MichaelnMoreno@gmail.com>
2023-05-24 04:54:02 -0500

    Finaalized Project ( Fully Responsive)

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.querySelector("#vegeta").addEventListener("mouseenter", vegeta)
 document.querySelector("#android17").addEventListener("mouseenter", android17)
 document.querySelector("#android18").addEventListener("mouseenter", android18)
 document.querySelector("#trunks").addEventListener("mouseenter", trunks)
+document.querySelector("#freiza").addEventListener("mouseenter", freiza)
+document.querySelector("#krillen").addEventListener("mouseenter", krillen)
 
 function goku () {
   document.querySelector('#video').style.backgroundImage = 'url(https://i.makeagif.com/media/12-27-2018/iM0Hhm.gif)'
@@ -42,6 +44,20 @@ function android18() {
 
 function trunks() {
   document.querySelector('#video').style.backgroundImage = 'url(https://media.tenor.com/6_7PTVdtB3IAAAAC/trunks-flippn-off.gif)'
+  document.querySelector("#video").style.border = "1rem solid black"
+  document.querySelector('#video').style.backgroundSize = 'cover'
+  document.querySelector('#video').style.backgroundRepeat = 'no-repeat'
+}
+
+function freiza() {
+  document.querySelector('#video').style.backgroundImage = 'url(https://media.tenor.com/gs8y_mJxLkUAAAAC/frieza-laugh.gif)'
+  document.querySelector("#video").style.border = "1rem solid black"
+  document.querySelector('#video').style.backgroundSize = 'cover'
+  document.querySelector('#video').style.backgroundRepeat = 'no-repeat'
+}
+
+function krillen() {
+  document.querySelector('#video').style.backgroundImage = 'url(https://media.tenor.com/FMwxkFJ8GrsAAAAC/dragon-ball-z-krillin.gif)'
   document.querySelector("#video").style.border = "1rem solid black"
   document.querySelector('#video').style.backgroundSize = 'cover'
   document.querySelector('#video').style.backgroundRepeat = 'no-repeat'
